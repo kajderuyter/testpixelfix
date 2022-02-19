a6a9a97011230149516dcb62191b2978d72b3c1c
Kaj <kajderuyter01@gmail.com>
2022-02-19 19:28:43 +0100

    bug fix

--- pages/license.jsx
+++ pages/license.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import Header from './components/Header.jsx'
 import LicenseMain from './components/LicenseMain'
-import logo from '../public/logo.png'
 
 function LicensePage(props) {
     return (
         <>
-            <Header logo={logo}/>
+            <Header />
             <LicenseMain />
         </>
     )
