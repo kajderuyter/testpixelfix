f3e6b474842056b4eb183720b78db234e4621fcc
Kaj <kajderuyter01@gmail.com>
2022-02-19 19:26:03 +0100

    possible image src fix

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import logo from '../../public/assets/images/logo.png'
 
 function Header(props) {
     return (
         <header>
-            <div className='header-brand'><Image src={props.logo} alt="logo" width={250} height={75}/><img src={'/logo.png'} alt='LOGO' /></div>
+            <div className='header-brand'><Image src={logo} alt="logo" width={250} height={75}/><img src={'/logo.png'} alt='LOGO' /></div>
             <div className='header-store-wrapper'>
                 <h2 className='header-store-name'>{props.name ? props.name.split('.myshopify.com')[0] : ''}</h2>
             </div>
