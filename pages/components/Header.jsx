cb30a13d06dfe8db5e8fa7b61049a22391a34265
Kaj de Ruyter <kajderuyter01@gmail.com>
2022-02-21 14:57:12 +0100

    Fixed logo alt

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -5,7 +5,7 @@ import Logo from '/public/images/logo.png'
 function Header(props) {
     return (
         <header>
-            <div className='header-brand'><Image src={Logo} width={250} height={50} alt='logootje'/></div>
+            <div className='header-brand'><Image src={Logo} width={250} height={50} alt='Logo'/></div>
             <div className='header-store-wrapper'>
                 <h2 className='header-store-name'>{props.name ? props.name.split('.myshopify.com')[0] : ''}</h2>
             </div>
